37a9485757bcca1efe4c28c82406432ebdeac809
ddayooung <dimmaculatey@gmail.com>
2020-10-15 11:47:05 +0900

    Feat : PostForm의  textArea 높이 변화

--- src/components/post/postForm/PostForm.js
+++ src/components/post/postForm/PostForm.js
@@ -1,8 +1,15 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useRef, useEffect } from 'react';
 import css from 'styled-jsx/css';
 
-const PostForm = ({ onAddPost }) => {
-  const [contents, setContents] = useState();
+const PostForm = (props) => {
+  const { onAddPost } = props;
+
+  const [contents, setContents] = useState('');
+  const [textAreaHeight, setTextAreaHeight] = useState(minHeight);
+
+  const RefTextArea = useRef(null);
+
+  const { minHeight = 100, lineHeight = 20, placeholder = '무슨 생각을 하고 계신가요?' } = props;
 
   const handlePostChange = useCallback((e) => {
     setContents(e.target.value);
@@ -13,16 +20,22 @@ const PostForm = ({ onAddPost }) => {
       e.preventDefault();
       onAddPost(contents);
       setContents('');
+      setTextAreaHeight(minHeight);
     },
     [onAddPost, contents]
   );
+
+  useEffect(() => {
+    setTextAreaHeight(RefTextArea.current.scrollHeight);
+  }, [contents]);
 
   return (
     <>
       <form className="write-form" onSubmit={handlePostSubmit}>
         <textarea
+          ref={RefTextArea}
           className="form-control input-lg"
-          placeholder="무슨 생각을 하고 계신가요?"
+          placeholder={placeholder}
           spellCheck="false"
           value={contents}
           onChange={handlePostChange}
@@ -31,47 +44,36 @@ const PostForm = ({ onAddPost }) => {
           공유하기
         </button>
       </form>
-      <style jsx>{StyledPostForm}</style>
+
+      <style jsx global>{`
+        .write-form {
+          margin-bottom: 100px;
+        }
+        .write-form > textarea.form-control {
+          min-height: ${textAreaHeight < 100 ? 100 : textAreaHeight}px;
+          line-height: ${lineHeight}px;
+          padding: 20px;
+          font-size: 18px;
+          resize: none;
+          border: none;
+          border-radius: 0.5rem;
+          transition: box-shadow ease-in-out 1s;
+        }
+        .write-form > textarea:focus {
+          box-shadow: #999999 0 0 50px;
+        }
+        .write-form > button.btn {
+          float: right;
+          margin-bottom: 0;
+          margin-top: 16px;
+          background-color: #3b5999;
+          color: #fffffe;
+          border: none;
+          font-weight: 800;
+        }
+      `}</style>
     </>
   );
 };
-
-const StyledPostForm = css`
-  .write-form {
-    margin-bottom: 100px;
-  }
-
-  .write-form > textarea.form-control {
-    min-height: 100px;
-    line-height: 20px;
-    padding: 20px;
-    font-size: 18px;
-    resize: none;
-    border: none;
-    border-radius: 0.5rem;
-    transition: box-shadow ease-in-out 1s;
-  }
-
-  .write-form > textarea:focus {
-    box-shadow: #999999 0 0 50px;
-  }
-
-  .comment-form > textarea.form-control {
-    min-height: 20px;
-    line-height: 20px;
-    border-radius: 0.5rem;
-    resize: none;
-  }
-
-  button.btn {
-    float: right;
-    margin-bottom: 0;
-    margin-top: 16px;
-    background-color: #3b5999;
-    color: #fffffe;
-    border: none;
-    font-weight: 800;
-  }
-`;
 
 export default PostForm;
